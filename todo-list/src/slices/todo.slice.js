3e0f51207d33092daa389e8ce940b755dbeb61ff
vipultiwari22 <tiwarivipul155@gmail.com>
2024-03-04 08:44:13 +0530

    update files

--- todo-list/src/slices/todo.slice.js
+++ todo-list/src/slices/todo.slice.js
@@ -7,9 +7,42 @@ const initialState = {
 const todoSlice = createSlice({
   name: "todo",
   initialState,
-  reducers:{
-    addTodo:(state,action)={
-        state.todo.push()
-    }
-  }
+  reducers: {
+    addTodo: (state, action) => {
+      let todoText = action.payload.todoText;
+      state.todo.push({
+        id: state.todo.length + 1,
+        todoData: todoText,
+        finished: false,
+      });
+    },
+    editTodo: (state, action) => {
+      let todo = action.payload.todo;
+      let todoText = action.payload.todoText;
+      state.todo = state.todo.map((t) => {
+        if (t.id == todo.id) {
+          todo.todoData = todoText;
+        }
+        return t;
+      });
+    },
+    todoFinished: (state, action) => {
+      let todo = action.payload.todo;
+      let isFinished = action.payload.isFinished;
+      const updatedList = state.todo.map((t) => {
+        if (t.todo == todo.id) {
+          todo.finished = isFinished;
+        }
+        return t;
+      });
+    },
+    deleteTodo: (state, action) => {
+      let todo = action.payload.todo;
+      state.todo = state.todo.filter((t) => t.id != todo.id);
+    },
+  },
 });
+
+export const { addTodo, editTodo, todoFinished, deleteTodo } =
+  todoSlice.actions;
+export default todoSlice.reducer;
--- todo-list/src/store.js
+++ todo-list/src/store.js
@@ -1,7 +1,15 @@
 import { createStore, combineReducers } from "redux";
 import todoReducers from "./reducers/todoReducers";
+// import todoReducer from "./slices/todo.slice";
+// import { configureStore } from "@reduxjs/toolkit";
 
 const reducres = combineReducers({ todo: todoReducers });
+
+// const store = configureStore({
+//   reducre: {
+//     todo: todoReducer,
+//   },
+// });
 
 const store = createStore(reducres);
 
