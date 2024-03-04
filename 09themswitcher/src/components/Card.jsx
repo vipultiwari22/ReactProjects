883802c7b2e9f73a129b709bffe98616d45d8853
vipultiwari22 <tiwarivipul155@gmail.com>
2024-03-04 08:57:00 +0530

    update files

--- 09themswitcher/src/components/Card.jsx
+++ 09themswitcher/src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card() {
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
-          src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src="09themswitcher\src\-hlrjzs.jpg"
           alt="product_image1"
         />
       </a>
